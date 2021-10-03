74170d67e79fba9dfddba16e176a56d3e5bc2aec
Muhammed Fathah <fathah1@gmail.com>
2021-10-03 21:21:24 +0400

    completed

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,30 +1,15 @@
 import classes from './NavBar.module.css'
 import React, {Component} from 'react'
+import MobileNavigation from './MobileNavigation'
+import Navigation from './Navigation'
+
 
 
 const NavBar = () => {
     return (  
         <div className={classes.NavBar} >
-         
-            <nav>
-                <ul>
-                    <li className="logo">
-                        <a href="/#home">fathah</a>
-                    </li>
-                    <li>
-                        <a href="#about" className="normalLink">About Me</a>
-                    </li>
-                    <li>
-                        <a href="/#skills" className="normalLink">Skills</a>
-                    </li>
-                    <li>
-                        <a href="/#projects" className="normalLink" > Projects</a>
-                    </li>
-                    <li>
-                        <a href="/#contactMe" className="normalLink" >Contact Me</a>
-                    </li>
-                </ul>
-            </nav>
+         <Navigation/>
+         <MobileNavigation/>
         </div>
     );
 }
